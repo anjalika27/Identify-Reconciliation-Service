f8fa648d2367a9d271fc00fa5de11bcabd78c0c8
Anjalika <anjalika.a@yelloskye.com>
2025-02-09 11:40:39 +0530

    db schema description

---
+++ modal/contact.js
@@ -0,0 +1,12 @@
+// table schema description
+
+/* 
+id: uuid string,
+email: string,
+phoneNumber: string,
+linkedId: string,
+linkPrecedence: string,
+createdAt: date,
+updatedAt: date,
+deletedAt: date,
+*/
