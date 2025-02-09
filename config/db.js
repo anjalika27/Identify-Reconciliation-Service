0b9a46cff765434a2e969368853d303ecc0d764e
Anjalika <anjalika.a@yelloskye.com>
2025-02-09 20:18:27 +0530

    added hosted db url in env

--- config/db.js
+++ config/db.js
@@ -1,16 +1,17 @@
 import { configDotenv } from "dotenv";
 import pkg from "pg";
 
-const {Pool}=pkg;
+const { Pool } = pkg;
 
 configDotenv();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    // user: process.env.DB_USER,
+    // host: process.env.DB_HOST,
+    // database: process.env.DB_DATABASE,
+    // password: process.env.DB_PASSWORD,
+    // port: process.env.DB_PORT
+    connectionString: process.env.DB_URL
 })
 
 export default pool;
