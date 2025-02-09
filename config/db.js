0c9ea522c02a44f6fb000c062c8f10a60c5f16f4
Anjalika <anjalika.a@yelloskye.com>
2025-02-09 20:41:25 +0530

    edited db configuration

--- config/db.js
+++ config/db.js
@@ -6,15 +6,15 @@ const { Pool } = pkg;
 configDotenv();
 
 const pool = new Pool({
-    // user: process.env.DB_USER,
-    // host: process.env.DB_HOST,
-    // database: process.env.DB_DATABASE,
-    // password: process.env.DB_PASSWORD,
-    // port: process.env.DB_PORT
-    connectionString: process.env.DB_URL,
-    ssl: {
-        rejectUnauthorized: false, // Allow self-signed SSL
-      },
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+    // connectionString: process.env.DB_URL,
+    // ssl: {
+    //     rejectUnauthorized: false, // Allow self-signed SSL
+    //   },
 })
 
 export default pool;
